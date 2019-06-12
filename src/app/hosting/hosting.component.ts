20843d7aec9eee34d7d67e4e659783d7bf208a68
Nacho Navarro <sukealo@gmail.com>
2019-06-12 12:17:38 -0400

    cambios en contacto

--- src/app/hosting/hosting.component.ts
+++ src/app/hosting/hosting.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HostingComponent implements OnInit {
 
-  constructor() { }
+  hosting: Array<any>;
+  sensitive : number = 0;
+  constructor() {
+
+    this.hosting = [
+      {
+        "plan" : "hosting 1",
+        "detalles" : "detalles de hosting",
+        "hidden" : true
+      },
+      {
+        "plan" : "hosting 2", 
+        "detalles" : "detalles de hosting",
+        "hidden" : true
+      },
+      {
+        "plan" : "hosting 3",
+        "detalles" : "detalles de hosting",
+        "hidden" : true
+      }
+    ]
+
+    setInterval(() => {
+      this.temporizador();
+    },4000)
+   }
 
   ngOnInit() {
   }
 
+
+  private temporizador(){
+    this.hosting.forEach((element, index) => {
+      element.hidden = true;
+      if(this.sensitive == index){
+          element.hidden = false;
+      }
+    });
+    if((this.sensitive+1) == this.hosting.length){
+      this.sensitive = 0
+    }else{
+      this.sensitive = this.sensitive + 1;
+    }
+  }
 }
