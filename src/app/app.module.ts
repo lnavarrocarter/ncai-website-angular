de539a53a7ba961ecb3f27fe9384e9852dc48c82
Luis LN. Navarro <lnavarro@adds.jordan.cl>
2019-06-12 17:44:30 -0400

    Cambio de layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainComponent } from './main/main.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
-import { TeamComponent } from './team/team.component';
 import { ServicesComponent } from './services/services.component';
 import { HostingComponent } from './hosting/hosting.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +23,6 @@ import { ContactComponent } from './contact/contact.component';
     NavbarComponent,
     MainComponent,
     MainLayoutComponent,
-    TeamComponent,
     ServicesComponent,
     HostingComponent,
     ProjectsComponent,
@@ -30,9 +30,10 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MDBBootstrapModule.forRoot(),
+    NgbModule.forRoot(),
     FormsModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
